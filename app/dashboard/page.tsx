5b6a2422e7180307df9f06477429f2c4cc162220
Abdisa.dev <meetabdisa@gmail.com>
2025-12-01 02:17:14 +0300

    redirect

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,12 @@
-export default function Dashboard() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/register");
+  }
   return <div>dashboard page</div>;
 }
