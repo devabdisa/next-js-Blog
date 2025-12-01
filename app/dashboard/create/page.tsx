bbef5e3b24ba5310fa64cd75cc83ed2a294a2bb0
Abdisa.dev <meetabdisa@gmail.com>
2025-12-01 15:07:09 +0300

    server actions

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,3 +1,4 @@
+import { handleSubmission } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -21,18 +22,18 @@ export default function CreateBlogRoute() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" action={handleSubmission}>
             <div className="flex flex-col gap-2">
               <Label>Title</Label>
-              <Input required type="text" placeholder="Title" />
+              <Input name="title" required type="text" placeholder="Title" />
             </div>
             <div className="flex flex-col gap-2">
               <Label>Content</Label>
-              <Textarea required placeholder="Content" />
+              <Textarea name="content" required placeholder="Content" />
             </div>
             <div className="flex flex-col gap-2">
               <Label>Image URL</Label>
-              <Input required type="url" placeholder="Image url" />
+              <Input name="url" required type="url" placeholder="Image url" />
             </div>
             <Button>Create Post</Button>
           </form>
