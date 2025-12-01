6dbeab151a42920b1c0a6811f4a1861251e39cd6
Abdisa.dev <meetabdisa@gmail.com>
2025-12-01 03:41:37 +0300

    Post creation UI

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,7 +1,43 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function CreateBlogRoute() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Card className="max-w-lg mx-auto">
+        <CardHeader>
+          <CardTitle>Create Post</CardTitle>
+          <CardDescription>
+            Create a new post to share with the World
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <Label>Title</Label>
+              <Input required type="text" placeholder="Title" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label>Content</Label>
+              <Textarea required placeholder="Content" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label>Image URL</Label>
+              <Input required type="url" placeholder="Image url" />
+            </div>
+            <Button>Create Post</Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
