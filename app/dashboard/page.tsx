73eabfc99335affe947058fb08dfb17bfd550a54
Abdisa.dev <meetabdisa@gmail.com>
2025-12-01 03:23:29 +0300

    middleware

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,13 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Dashboard() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-
-  if (!user) {
-    return redirect("/api/auth/register");
-  }
-  return <div>dashboard page</div>;
+  return (
+    <div >
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium">Your Blog Articles</h2>
+        <Link className={buttonVariants()} href="/dashboard/create">Create Post</Link>
+      </div>
+    </div>
+  );
 }
