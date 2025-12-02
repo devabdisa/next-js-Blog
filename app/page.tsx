cd84bb013336a255c4ee17203a9fdcdacd4d1314
Abdisa.dev <meetabdisa@gmail.com>
2025-12-03 00:29:47 +0300

    Suspense for the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import { BlogpostCard } from "@/components/general/BlogpostCard";
 import { prisma } from "./utils/db";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 async function getData() {
   const data = await prisma.blogPost.findMany({
@@ -10,25 +13,31 @@ async function getData() {
       authorName: true,
       id: true,
       createdAt: true,
+      authorId: true,
+      updatedAt: true,
     },
   });
   return data;
 }
 
 export default async function Home() {
-  const data = await getData();
   return (
     <div className="py-6">
       <h1 className="text-3xl font-bold tracking-tight mb-8 ">Latest Posts</h1>
+      <Suspense fallback={<Skeleton className="w-full h-[400px]"/>}>
+        <BlogPosts />
+      </Suspense>
+    </div>
+  );
+}
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data.map((item) => (
-          <>
-            {/* <h1 key={item.title}>{item.title}</h1>
-            <h1 key={item.content}>{item.content}</h1> */}
-          </>
-        ))}
-      </div>
+async function BlogPosts() {
+  const data = await getData();
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {data.map((item) => (
+        <BlogpostCard data={item} key={item.id} />
+      ))}
     </div>
   );
 }
